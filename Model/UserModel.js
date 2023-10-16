e62c411f58dbfa1b98761a843b9fe4a0fd33df70
nono <noamcurely@gmail.com>
2023-10-16 07:46:18 +0200

    fichier qui crer et defini la table user ainsi que ces colonnes

---
+++ Model/UserModel.js
@@ -0,0 +1,36 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../Config/ConnectDb");
+
+//définition des colonnes pour la table user
+const User = sequelize.define('user', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true,
+        unique: true
+    },
+    firstname:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    lastname: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    role: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+}, {});
+
+module.exports = User;
